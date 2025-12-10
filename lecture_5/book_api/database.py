8faefad10f329f3751653cd8aa337a6e0e5e5579
Artyom Dordyuk <a.dorduke@gmail.com>
2025-12-10 20:52:02 +0300

    refresh code

--- lecture_5/book_api/database.py
+++ lecture_5/book_api/database.py
@@ -12,7 +12,7 @@ engine = create_engine(
     connect_args={"check_same_thread": False},
 )
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False,autocommit=False)
+SessionLocal = sessionmaker(bind=engine)
 
 def get_db():
     db = SessionLocal()
--- lecture_5/book_api/main.py
+++ lecture_5/book_api/main.py
@@ -10,22 +10,24 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Book API")
 
-@app.post("/books/", response_model=schemas.Book)
+@app.post("/books/", response_model=schemas.Book, description="Add a new book")
 def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
     return crud.create_book(db, book)
 
-@app.get("/books/", response_model=list[schemas.Book])
+@app.get("/books/", response_model=list[schemas.Book], description="Get all books")
 def read_books(db: Session = Depends(get_db)):
     return crud.get_books(db)
 
-@app.get("/books/{book_id}", response_model=schemas.Book)
-def read_book(book_id: int, db: Session = Depends(get_db)):
-    book = crud.get_book(db, book_id)
-    if not book:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return book
 
-@app.put("/books/{book_id}", response_model=schemas.Book)
+@app.delete("/books/{book_id}",description="Delete a book by ID")
+def delete_book(book_id: int, db: Session = Depends(get_db)):
+    ok = crud.delete_book(db, book_id)
+    if not ok:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return {"message": "Book deleted"}
+
+
+@app.put("/books/{book_id}", response_model=schemas.Book, description="Update book details")
 def update_book(book_id: int, new_data: schemas.BookUpdate, db: Session = Depends(get_db)):
     book = crud.update_book(db, book_id, new_data)
     if not book:
@@ -33,13 +35,6 @@ def update_book(book_id: int, new_data: schemas.BookUpdate, db: Session = Depend
     return book
 
 
-@app.delete("/books/{book_id}")
-def delete_book(book_id: int, db: Session = Depends(get_db)):
-    ok = crud.delete_book(db, book_id)
-    if not ok:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return {"message": "Book deleted"}
-
-@app.get("/books/search/", response_model=list[schemas.Book])
+@app.get("/books/search/", response_model=list[schemas.Book], description="Search books by title, author, or year")
 def search(title: str = None, author: str = None, year: int = None, db: Session = Depends(get_db)):
     return crud.search_books(db, title, author, year)
